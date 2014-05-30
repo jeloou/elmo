129e0b6df4a36d1a05b8cc71a0e0cd3ddb144ddb
Joseph Núñez <toctochello@gmail.com>
2014-05-30 00:47:39 -0430

    I added this example by error, just removed my keys

--- examples/client.js
+++ examples/client.js
@@ -1,8 +1,8 @@
 var elmo = require('../');
 
 var client = elmo.OAuth({
-  appKey: 'V4dnUFZLY4gk',
-  appSecret: 'az02uZiyIby91slluxj2HiJBF1I5aq9f',
+  appKey: '<your app key>',
+  appSecret: '<your app secret>',
   urls: {
     requestToken: 'http://www.plurk.com/OAuth/request_token',
     accessToken: 'http://www.plurk.com/OAuth/access_token',
@@ -15,14 +15,11 @@ client.on('error', function(err) {
 });
 
 client.requestToken(function(err, data) {
-  console.log('data: ', data);
   process.stdin.on('readable', function() {
     var verifier = process.stdin.read();
     
     if (verifier != null) {
       verifier = verifier.toString();
-      
-      console.log('verifier: ', verifier);
       client.accessToken(verifier.substr(0, verifier.length-1), function(err, token) {
 	console.log(token);
       });
