90d3c8595e32e8d0b1b49422998c21cae2af68ca
Joseph Núñez <toctochello@gmail.com>
2014-05-30 02:33:58 -0430

    Updated the basic client example

--- examples/client.js
+++ examples/client.js
@@ -11,22 +11,30 @@ var client = elmo.OAuth({
 });
 
 client.on('error', function(err) {
-  console.log(this);
+  console.log(err);
 });
 
 client.requestToken(function(err, data) {
+  console.log('Now go to', client.authorize());
+  console.log('and introduce the code:');
+  
   process.stdin.on('readable', function() {
     var verifier = process.stdin.read();
     
     if (verifier != null) {
       verifier = verifier.toString();
       client.accessToken(verifier.substr(0, verifier.length-1), function(err, token) {
+	/* Now you have an access token */
 	console.log(token);
+	
+	/* and the client can make requests */
+	client.request('get', 'http://www.plurk.com/APP/Profile/getOwnProfile', function(res) {
+	  res.setEncoding('utf8');
+	  res.on('data', function(data) {
+	    console.log('me: ', data);
+	  });
+	});
       });
     }
   });
 });
-
-
-
-
